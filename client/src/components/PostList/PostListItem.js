a89520a5b4fc629edb0e2ce48a5587ed7f355fa7
neupanesabin <54862359+neupanesabin@users.noreply.github.com>
2019-11-24 23:47:42 -0600

    Completed Activity-10

--- client/src/components/PostList/PostListItem.js
+++ client/src/components/PostList/PostListItem.js
@@ -4,7 +4,7 @@ import slugify from 'slugify';
 import './styles.css'
 
 const PostListItem = props => {
-    const { post, clickPost,deletePost } = props;
+    const { post, clickPost,deletePost, editPost } = props;
     const history = useHistory();
 
     const handleClickPost = post => {
@@ -13,6 +13,10 @@ const PostListItem = props => {
         clickPost(post);
         history.push(`/posts/${slug}`);
     }
+    const handlEditPost= post => {
+        editPost(post);
+        history.push(`/edit-post/$(post.id)`);
+    };
 
     return (
         <div>
@@ -22,6 +26,7 @@ const PostListItem = props => {
         </div>
         <div className="postControls">
             <button onClick={()=> deletePost(post)}>Delete</button>
+            <button onClick={()=> handleClickPost(post)}>Edit</button>
         </div>
         </div>
     )
